5c176409095e7a053e02881be1eb61994df9ae8c
Erik Rosness <erik@rosness.no>
2022-12-16 10:59:46 +0100

    Save remains after prototyping remote debug.

--- tst.py
+++ tst.py
@@ -1,1 +1,25 @@
-pypy
+print ("pypypypy!XXXX")
+
+import debugpy
+import serial
+
+# 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
+#debugpy.listen(5678)
+#print("Waiting for debugger attach")
+#debugpy.wait_for_client()
+#debugpy.breakpoint()
+#print('break on this line')
+
+ser = serial.Serial('/dev/ttyUSB0',920000)
+s = ser.read(100)
+
+
+for loopcnt in range(29):
+    line = ser.readline()        
+    print(line)
+
+
+
+print ("end")
+
+
