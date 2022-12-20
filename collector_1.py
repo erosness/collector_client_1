f27f71cfb9839a171427bc094c110f7df4946155
Erik Rosness <erik@rosness.no>
2022-12-20 11:44:28 +0100

    Just able to log torque data. Prototype, needs cleanup.

--- collector_1.py
+++ collector_1.py
@@ -2,15 +2,17 @@ import requests
 import json
 import time
 import datetime
+import serial
 
 unit_serial = "A"
 logger_id = "10.0.0.111"
 timestamp = time.localtime
 
+#url_host = "http://collector.rosness.no:8001/" 
 url_host = "http://localhost:8001/" 
-unit_url_path = "probaker/api/unit/" 
-session_url_path = "probaker/api/session/" 
-torque_url_path = "probaker/api/torque/" 
+unit_url_path = "probaker/api/unit.php" 
+session_url_path = "probaker/api/session.php" 
+torque_url_path = "probaker/api/torque.php" 
 unit_serial_parameter = "unit_serial=" + unit_serial
 logger_id_parameter = "logger_id=" + logger_id
 
@@ -19,6 +21,7 @@ response_payload = requests.get(url_string)
 unit_info = json.loads(response_payload.text)
 
 unit_id = unit_info["unit_id"]
+unit_id_parameter = "unit_id=" + unit_id
 
 ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
@@ -34,38 +37,56 @@ session_data = {
 
 session_data_str = json.dumps(session_data)
 
-url_string = url_host + session_url_path + unit_id
+url_string = url_host + session_url_path + "?" + unit_id_parameter
 response_payload = requests.post(url_string, session_data_str)
 session_info = json.loads(response_payload.text)
 
 session_id = session_info["session_id"]
+session_id_parameter = "session_id=" + session_id
 
 
 
 #print(y["unit_id"])
 
 
-for loopcnt in range(9):
-    print("Før -Loopcnt=", loopcnt)
-
-    ts = time.time()
-    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-
-    torque_data = {
-        "session_id" : session_id,
-        "log_time" : st,
-        "machine_time" : 50000
-    }
-
-    torque_data_str = json.dumps(torque_data)
-
-    url_string = url_host + torque_url_path + session_id
-    response_payload = requests.post(url_string, torque_data_str)
-    log_info = json.loads(response_payload.text)
+ser = serial.Serial('/dev/ttyUSB0', 921600 )
+ser.reset_input_buffer()
+machine_time_counter = 50000
 
 
-    print("Etter - Loopcnt=", loopcnt, " data: ", log_info)
-    time.sleep(1.1)
+for loopcnt in range(99):
+#    print("Før -Loopcnt=", loopcnt)
+
+    line = ser.readline()
+    try:
+        torque_data = json.loads(line)
+    except:
+        print("JSON parse error")
+        torque_data = {
+            "torque":"nothing",
+            "time" : 0
+        }
+    else:
+        ts = time.time()
+        st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+
+        torque_log = {
+            "session_id" : session_id,
+            "log_time" : st,
+            "machine_time" : torque_data["time"],
+            "torque" : torque_data["torque"]
+        }
+
+        machine_time_counter+=50
+        torque_data_str = json.dumps(torque_log)
+
+        url_string = url_host + torque_url_path + "?" + session_id_parameter
+        response_payload = requests.post(url_string, torque_data_str)
+#        log_info = json.loads(response_payload.text)
+
+
+    print("Etter - Loopcnt=", loopcnt, " data: ")
+#    time.sleep(1.1)
 
 print ("end")
 
